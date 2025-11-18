d2e387589464ccc940e9122e6bb21e05d18c0548
TemaTut <pyrkin2019@gmail.com>
2025-11-18 04:54:55 +0300

    reset fix qwen.py

--- app/generators/qwen.py
+++ app/generators/qwen.py
@@ -6,32 +6,27 @@ from PIL import Image
 from diffusers import DiffusionPipeline
 
 
+
 def generate_image(
-    task_id: UUID,
-    image_1: bytes,
-    image_2: bytes | None = None,
-    prompt: str = "",
-    negative: str = "",
-    num_inference_steps: int = 30,
+        task_id: UUID,
+        image_1: bytes,
+        image_2: bytes | None = None,
+        prompt: str = "",
+        negative: str = "",
+        num_inference_steps: int = 30,
 ):
     cache_path = Path(__file__).parent.parent.parent / "models"
+    torch_dtype = torch.bfloat16
 
-    # ✔ Balanced Offload — работает на A6000
     pipe = DiffusionPipeline.from_pretrained(
         str(cache_path / "Qwen/Qwen-Image-Edit-2509"),
-        torch_dtype=torch.float16,
-        device_map="balanced",
+        torch_dtype=torch_dtype,
     )
-
-    # экономный attention
-    pipe.enable_xformers_memory_efficient_attention()
-
+    pipe.enable_model_cpu_offload()
     pipe.set_progress_bar_config(disable=None)
-
     images = [Image.open(BytesIO(image_1)).convert("RGB")]
     if image_2:
         images.append(Image.open(BytesIO(image_2)).convert("RGB"))
-
     inputs = {
         "image": images,
         "prompt": prompt,
@@ -40,10 +35,8 @@ def generate_image(
         "num_inference_steps": num_inference_steps,
         "num_images_per_prompt": 1,
     }
-
     with torch.inference_mode():
         output = pipe(**inputs)
         image = output.images[0]
-
         save_path = Path(__file__).parent.parent.parent / "output" / f"{task_id}.png"
         image.save(save_path)
